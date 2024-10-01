d3e7c321476baa7eb8efcb0fa83210dd9793c56a
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-10-01 16:22:59 -0300

    Aula 20 - Acessando a rota de pedidos

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, jsonify, request
 from src.main.http_types.http_request import HttpRequest
 
+from src.main.composer.register_order_composer import registry_order_composer
+
 delivery_routes_bp = Blueprint("delivery_routes", __name__)
 
 
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
-    print(request.json)
-    http_request = HttpRequest(body=request.json, header=request.headers)
+    user_case = registry_order_composer()
+    http_request = HttpRequest(body=request.json)
+    response = user_case.registry(http_request)
 
-    # enviar o http_request para a nossa lógica
-    # logica ira retornar http_response
-
-    return jsonify({"ola": "mundo"}), 200
+    return jsonify(response.body), response.status_code

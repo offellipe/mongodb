cfdb1914d3c92b0ec14fcbcd4100c1d5b7e21cf1
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-10-01 14:36:22 -0300

    Aula 15 - Implementando pasta main e framework

---
+++ src/main/routes/delivery_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify, request
+
+delivery_routes_bp = Blueprint("delivery_routes", __name__)
+
+
+@delivery_routes_bp.route("/delivery/order", methods=["POST"])
+def registry_order():
+    print(request.json)
+    return jsonify({"ola": "mundo"}), 200
